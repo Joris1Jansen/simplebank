f53a33c6a8299b36eb5553e3a9f0b1dc73f8dc24
Joris Jansen <j.jansen@ispnext.com>
2022-08-16 21:17:45 +0200

    Main test && store

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -40,23 +40,58 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 // TransferTxParams contains the input parameters of the transfer transaction
 type TransferTxParams struct {
-	fromAccountId int64 `json:"from_account_id"`
-	toAccountId   int64 `json:"to_account_id"`
+	FromAccountId int64 `json:"from_account_id"`
+	ToAccountId   int64 `json:"to_account_id"`
 	Amount        int64 `json:"amount"`
 }
 
 // TransferTxResult contains the output of the transfer transaction
 type TransferTxResult struct {
-    Transfer Transfer `json:"transfer"`
-    FromAccount Account `json:"from_account"`
-    ToAccount Account `json:"to_account"`
-    FromEntry Entry `json:"from_entry"`
-    ToEntry Entry `json:"to_entry"`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries and update accounts' balance
 // within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult error) {
+	var result TransferTxResult
 
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID:   arg.ToAccountId,
+			Amount:        arg.Amount,
+		})
+        if err != nil {
+            return err
+        }
+
+        result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+            AccountID: arg.FromAccountId,
+            Amount: -arg.Amount,
+        })
+        if err != nil {
+            return err
+        }
+
+        result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+            AccountID: arg.ToAccountId,
+            Amount: -arg.Amount,
+        })
+        if err != nil {
+            return err
+        }
+
+        // TODO: update accounts` balance
+
+		return nil
+	})
+
+	return result, err
 }
